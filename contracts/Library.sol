82b6990a44b295f61d9f4c46a487e8946654b625
VTAkbay <vahidturabakbay1@gmail.com>
2022-09-28 13:48:30 +0300

    feat: create addBook function

--- contracts/Library.sol
+++ contracts/Library.sol
@@ -1,30 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "./Book.sol";
+import "./Utils.sol";
+
 contract Library {
+    using BookUtils for Book;
+    using StringUtils for string;
+
     address owner;
 
     constructor() {
         owner = msg.sender;
     }
 
-    struct Book {
-        address owner;
-        string title;
-        string authorFirstName;
-        string authorLastName;
-        string isbn;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can do that");
+        _;
     }
 
-    Book[] public books;
+    struct Copy {
+        bool _isValid;
+        address holder;
+    }
+
+    mapping(string => Book) public bookByIsbn;
+    string[] public bookIsbns;
+
+    uint256 private _nextCopyId;
+    mapping(uint256 => Copy) public copies;
 
     function addBook(
         string memory title,
         string memory authorFirstName,
         string memory authorLastName,
         string memory isbn
-    ) public {
-        books.push(Book(owner, title, authorFirstName, authorLastName, isbn));
+    ) public onlyOwner {
+        Book storage book = bookByIsbn[isbn];
+        book._isValid = true;
+        book._isbn = isbn;
+        book._owner = msg.sender;
+        book._title = title;
+        book._authorFirstName = authorFirstName;
+        book._authorLastName = authorLastName;
+
+        bookIsbns.push(isbn);
+    }
     }
 
     function deleteBook(uint256 index) public {
