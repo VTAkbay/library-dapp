852382be68dd9f267a643d8b975887580874a427
VTAkbay <vahidturabakbay1@gmail.com>
2022-09-28 13:51:09 +0300

    feat: create utils contract library

---
+++ contracts/Utils.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+library StringUtils {
+    function equals(string memory a, string memory b)
+        public
+        pure
+        returns (bool)
+    {
+        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+    }
+}
