06a230521eaf862816ba3edb13149136e91cf3c0
VTAkbay <vahidturabakbay1@gmail.com>
2022-09-28 13:54:14 +0300

    feat: create test suites for Library contract

--- test/Library.ts
+++ test/Library.ts
@@ -3,52 +3,117 @@ import { expect } from "chai";
 import { Library } from "../typechain-types";
 import { PromiseOrValue } from "../typechain-types/common";
 
-describe("Test Library contract", function () {
-  let contract: Library;
-  let owner: { address: PromiseOrValue<string> };
+const data = {
+  book: {
+    title: "Test Title",
+    authorFirstName: "Turab",
+    authorLastName: "Akbay",
+    isbn: "978-3-16-148410-0",
+  },
+  defaultHolder: "0x0000000000000000000000000000000000000000",
+};
 
-  beforeEach(async function () {
-    // Create the smart contract object to test from
+let contract: Library;
+let owner: { address: PromiseOrValue<string> };
+
+describe("Test Library contract", function () {
+  before(async function () {
     [owner] = await ethers.getSigners();
-    const LibraryContract = await ethers.getContractFactory("Library");
+
+    const BookContract = await ethers.getContractFactory("BookUtils");
+    const bookContract = await BookContract.deploy();
+    await bookContract.deployed();
+
+    const UtilsContract = await ethers.getContractFactory("StringUtils");
+    const utilsContract = await UtilsContract.deploy();
+    await utilsContract.deployed();
+
+    const LibraryContract = await ethers.getContractFactory("Library", {
+      libraries: {
+        BookUtils: bookContract.address,
+        StringUtils: utilsContract.address,
+      },
+    });
     contract = await LibraryContract.deploy();
   });
 
-  async function addBook() {
-    const addBookTest = await contract.addBook(
-      "Test Title",
-      "Turab",
-      "Akbay",
-      "978-3-16-148410-0"
-    );
-
-    // Wait until the transaction is mined
-    await addBookTest.wait();
-  }
-
   it("Add book should work", async function () {
-    addBook();
+    // Add book
+    const addBookTest = await contract.addBook(
+      data.book.title,
+      data.book.authorFirstName,
+      data.book.authorLastName,
+      data.book.isbn
+    );
+    await addBookTest.wait();
 
-    const book = await contract.books([0]);
+    // Check if book has been added
+    const book = await contract.bookByIsbn(data.book.isbn);
 
-    expect(book.owner).to.equal(owner.address);
-    expect(book.title).to.equal("Test Title");
-    expect(book.authorFirstName).to.equal("Turab");
-    expect(book.authorLastName).to.equal("Akbay");
-    expect(book.isbn).to.equal("978-3-16-148410-0");
+    // Check the book if the values are correct
+    expect(book._isValid).to.equal(true);
+    expect(book._isbn).to.equal(data.book.isbn);
+    expect(book._owner).to.equal(owner.address);
+    expect(book._title).to.equal(data.book.title);
+    expect(book._authorFirstName).to.equal(data.book.authorFirstName);
+    expect(book._authorLastName).to.equal(data.book.authorLastName);
   });
 
-  it("Delete book should work", async function () {
-    addBook();
+  it("Add copy should work", async function () {
+    // Add copy for book
+    const addCopy = await contract.addCopy(data.book.isbn);
+    await addCopy.wait();
 
-    const deleteBook = await contract.deleteBook("0");
+    // Check if the copy has been created
+    const copy = await contract.copies(0);
+    expect(copy._isValid).to.equal(true);
+  });
 
-    await deleteBook.wait();
+  it("Issue copy should work", async function () {
+    // Issue copy
+    const issueCopy = await contract.issueCopy(
+      data.book.isbn,
+      0,
+      owner.address
+    );
+    await issueCopy.wait();
 
-    try {
-      await contract.books(0);
-    } catch (error: any) {
-      expect(error.data).to.equal("0x");
-    }
+    // Check if the copy has been issued
+    const copy = await contract.copies(0);
+    expect(copy.holder).to.equal(owner.address);
+  });
+
+  it("Return copy should work", async function () {
+    // Return the copy
+    const returnCopy = await contract.returnCopy(
+      data.book.isbn,
+      0,
+      owner.address
+    );
+    await returnCopy.wait();
+
+    // Check if the copy has been returned
+    const copy = await contract.copies(0);
+    expect(copy.holder).to.equal(data.defaultHolder);
+  });
+
+  it("Delete copy should work", async function () {
+    // Delete copy of book
+    const deleteCopy = await contract.deleteCopy(data.book.isbn, 0);
+    await deleteCopy.wait();
+
+    // Check if the copy has been deleted
+    const copy = await contract.copies(0);
+    expect(copy._isValid).to.equal(false);
+  });
+
+  it("Remove book should work", async function () {
+    // Remove book
+    const removeBook = await contract.removeBook(data.book.isbn);
+    await removeBook.wait();
+
+    // Check if book is removed
+    const book = await contract.bookByIsbn(data.book.isbn);
+    expect(book._isValid).to.equal(false);
   });
 });
