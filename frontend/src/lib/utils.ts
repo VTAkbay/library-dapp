a443fc3b8e2c88a0c5dcb385d328fb0c349cc322
VTAkbay <vahidturabakbay1@gmail.com>
2022-10-06 20:13:12 +0300

    feat: imports for contract and abi

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,4 +1,9 @@
 import axios from "axios";
+import ABI from "../library-abi.json";
+
+export const contractInterface = ABI;
+
+export const contractAdress = "0x51D903AB400053b60C1E32Dc6cd9Dba9FB1Ca604";
 
 export const openInNewTab = (url: string) => {
   window.open(url, "_blank", "noopener,noreferrer");
@@ -9,3 +14,11 @@ export async function mockImg() {
     return res.request.responseURL;
   });
 }
+
+export const isDev =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
+export const libraryContract = {
+  addressOrName: contractAdress,
+  contractInterface: contractInterface,
+};
