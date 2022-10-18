5171c90bbc43888b4de7e56528273988efae1651
VTAkbay <vahidturabakbay1@gmail.com>
2022-10-18 22:54:12 +0300

    refactor: update deploy script & without libraries

--- scripts/deployLibrary.ts
+++ scripts/deployLibrary.ts
@@ -1,28 +1,7 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const BookContract = await ethers.getContractFactory("BookUtils");
-  const bookContract = await BookContract.deploy();
-  console.log("Deployed Book, waiting for confirmation");
-
-  await bookContract.deployed().then(() => {
-    console.log("Book has been confirmed");
-  });
-
-  const UtilsContract = await ethers.getContractFactory("StringUtils");
-  const utilsContract = await UtilsContract.deploy();
-  console.log("Deployed Utils, waiting for confirmation");
-
-  await utilsContract.deployed().then(() => {
-    console.log("Utils has been confirmed");
-  });
-
-  const Library = await ethers.getContractFactory("Library", {
-    libraries: {
-      BookUtils: bookContract.address,
-      StringUtils: utilsContract.address,
-    },
-  });
+  const Library = await ethers.getContractFactory("Library");
   const library = await Library.deploy();
   console.log("Deployed Library, waiting for confirmation");
 
@@ -30,7 +9,7 @@ async function main() {
     console.log("Library has been confirmed");
   });
 
-  console.log("Blog deployed to:", library.address);
+  console.log("Library deployed to:", library.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
