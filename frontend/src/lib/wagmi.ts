b5d5d401a9bb443a60555f4bdfa3a28f0e753dd2
VTAkbay <vahidturabakbay1@gmail.com>
2022-10-25 23:37:37 +0300

    feat: update wagmi with ganacheChain

--- frontend/src/lib/wagmi.ts
+++ frontend/src/lib/wagmi.ts
@@ -1,11 +1,36 @@
-import { chain, configureChains, createClient } from "wagmi";
+import { Chain, chain, configureChains, createClient } from "wagmi";
 
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { isDev } from "./utils";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { publicProvider } from "wagmi/providers/public";
 
+export const ganacheChain: Chain = {
+  id: 1337,
+  name: "Ganache",
+  network: "ganache",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ethereum",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: "HTTP://127.0.0.1:8545",
+  },
+  testnet: true,
+};
+
 export const { chains, provider } = configureChains(
-  [chain.goerli],
-  [publicProvider()]
+  [isDev ? ganacheChain : chain.goerli],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => {
+        if (chain.id !== ganacheChain.id) return null;
+        return { http: chain.rpcUrls.default };
+      },
+    }),
+    publicProvider(),
+  ]
 );
 
 export const { connectors } = getDefaultWallets({
