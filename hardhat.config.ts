96d7d60e9b8a6b0f80cdf1bde3fc535fcbc45b8f
VTAkbay <vahidturabakbay1@gmail.com>
2022-10-25 23:34:50 +0300

    feat: add new network ganache to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,26 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-etherscan";
+
+import { HardhatUserConfig } from "hardhat/config";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const ALCHEMY_HTTPS = process.env.ALCHEMY_HTTPS;
+const GANACHE_RPC = process.env.GANACHE_RPC;
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const GANACHE_WALLET_PRIVATE_KEY = process.env.GANACHE_WALLET_PRIVATE_KEY;
 
-if (!ALCHEMY_HTTPS || !WALLET_PRIVATE_KEY || !ETHERSCAN_API_KEY) {
+if (
+  !ALCHEMY_HTTPS ||
+  !GANACHE_RPC ||
+  !WALLET_PRIVATE_KEY ||
+  !GANACHE_WALLET_PRIVATE_KEY ||
+  !ETHERSCAN_API_KEY
+) {
   throw new Error(
-    "Missing ALCHEMY_HTTPS, WALLET_PRIVATE_KEY or ETHERSCAN_API_KEY environment "
+    "Missing ALCHEMY_HTTPS, GANACHE_RPC, WALLET_PRIVATE_KEY, GANACHE_WALLET_PRIVATE_KEY or ETHERSCAN_API_KEY environment "
   );
 }
 
@@ -21,6 +30,10 @@ const config: HardhatUserConfig = {
     goerli: {
       url: ALCHEMY_HTTPS,
       accounts: [WALLET_PRIVATE_KEY!],
+    },
+    ganache: {
+      url: GANACHE_RPC,
+      accounts: [GANACHE_WALLET_PRIVATE_KEY],
     },
   },
   etherscan: {
